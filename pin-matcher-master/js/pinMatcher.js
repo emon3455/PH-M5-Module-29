dbd241b63f51a1bf02acbea900d2a9342868e843
emon3455 <emon.mhk69@gmail.com>
2023-02-15 01:25:07 +0600

    random pin code done

--- pin-matcher-master/js/pinMatcher.js
+++ pin-matcher-master/js/pinMatcher.js
@@ -1,8 +1,17 @@
 function getPin(){
+    const random  = generatePin();
+    const randomString = random + "";
 
+    if(randomString.length === 4){
+        return random;
+    }
+    else{
+        return getPin();
+    }
 }
 
 function generatePin(){
     const random = Math.round(Math.random*1000);
-     
+    return random; 
 }
+
