42700dea1c27961dd6ebb1b166e7115ef8a839da
emon3455 <emon.mhk69@gmail.com>
2023-02-15 01:54:06 +0600

    calculator input done

--- pin-matcher-master/js/pinMatcher.js
+++ pin-matcher-master/js/pinMatcher.js
@@ -22,3 +22,30 @@ document.getElementById("get-pin").addEventListener("click" , function(){
     const displayField = document.getElementById("pin-display");
     displayField.value=pin;
 });
+
+
+// display calculator digit:
+document.getElementById("calculator").addEventListener("click" , function(event){
+
+        const numbers = event.target.innerText;
+
+        const inputField = document.getElementById("calc-input");
+        const prevNum = inputField.value;
+
+        if(isNaN(numbers)){
+            if(numbers=="C"){
+                inputField.value="";
+            }
+            else if(numbers == "<"){
+                let ar = prevNum.split("");
+                ar.pop();
+                let remain = ar.join("");
+                inputField.value = remain;
+            }
+        }
+        else{
+            const newNumber = prevNum + numbers;
+            inputField.value = newNumber;
+        }
+
+});
